e3f4a4c06324bcd2915ed4b7012268d088eefb90
Aizwal <amanjaiswal144181@gmail.com>
2025-02-02 21:13:19 +0530

    Update api.js

--- mobile/app/services/api.js
+++ mobile/app/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "http://192.168.1.4:8000"; // Update with your backend URL
+const API_BASE = "localhost:8000"; // Update with your backend URL
 
 export const storeClipboardText = async (text) => {
     try {
